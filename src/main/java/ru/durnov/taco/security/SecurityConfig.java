45f19d6d05774624b6132abd32b626816c955019
Alexej <door1975@yandex.ru>
2020-09-13 22:11:10 +0300

    Изучаю security

---
+++ src/main/java/ru/durnov/taco/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package ru.durnov.taco.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    public static final String DEF_USERS_BY_USERNAME_QUERY =
+            "select username,password,enabled " +
+                    "from users " +
+                    "where username = ?";
+    public static final String DEF_AUTHORITIES_BY_USERNAME_QUERY =
+            "select username,authority " +
+                    "from authorities " +
+                    "where username = ?";
+
+    public static final String DEF_GROUP_AUTHORITIES_BY_USERNAME_QUERY =
+            "select g.id, g.group_name, ga.authority " +
+                    "from groups g, group_members gm, group_authorities ga " +
+                    "where gm.username = ? " +
+                    "and g.id = ga.group_id " +
+                    "and g.id = gm.group_id";
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.ldapAuthentication().userSearchFilter("(uid={0})").groupSearchFilter("member={0}");
+    }
+}
