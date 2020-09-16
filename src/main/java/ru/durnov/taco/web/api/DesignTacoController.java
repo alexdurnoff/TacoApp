017251391472c5fec6ce3dd9ae08cb5bb0faf051
alexej <door1975@yandex.ru>
2020-09-16 19:08:28 +0300

    Когда дошел до hypermedia, в книге началась жесть полная... Разбираемся...

--- src/main/java/ru/durnov/taco/web/api/DesignTacoController.java
+++ src/main/java/ru/durnov/taco/web/api/DesignTacoController.java
@@ -1,14 +1,21 @@
 package ru.durnov.taco.web.api;
 
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
-import org.springframework.hateoas.server.EntityLinks;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.hateoas.EntityLinks;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 import ru.durnov.taco.Taco;
 import ru.durnov.taco.data.TacoRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -25,18 +32,37 @@ public class DesignTacoController {
     }
 
     @GetMapping("/recent")
-    public Iterable<Taco> recentTacos(){
-        PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending());
-        return tacoRepo.findAll(page.getSort());
+    public Resources<Resource<Taco>> recentTacos() {
+        PageRequest page = PageRequest.of(
+                0, 12, Sort.by("createdAt").descending());
+        List<Taco> tacos = tacoRepo.findAll(page).getContent();
+        Resources<Resource<Taco>> recentResources = Resources.wrap(tacos);
+        recentResources.add(
+                new Link("http://localhost:8080/design/recent", "recents"));
+        return recentResources;
     }
 
     @GetMapping("/{id}")
-    public Taco tacoById(@PathVariable("id") Long id){
+    public ResponseEntity<Taco> tacoById(@PathVariable("id") Long id){
         Optional<Taco> optTaco = tacoRepo.findById(id);
-        if(optTaco.isPresent()){
-            return optTaco.get();
+        return optTaco.map(taco -> new ResponseEntity<>(taco, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Taco postTaco(@RequestBody Taco taco){
+        return tacoRepo.save(taco);
+    }
+
+    @DeleteMapping("/{tacoId}")
+    @ResponseStatus(code=HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable ("tacoId") Long tacoId){
+        try {
+            tacoRepo.deleteById(tacoId);
+        } catch (EmptyResultDataAccessException exception){
+
         }
-        return null;
+
     }
 
 
---
+++ src/main/java/ru/durnov/taco/web/api/OrderController.java
@@ -0,0 +1,74 @@
+package ru.durnov.taco.web.api;
+
+
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.durnov.taco.Order;
+import ru.durnov.taco.data.OrderRepository;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+    private OrderRepository orderRepo;
+
+    public OrderController(OrderRepository orderRepo){
+        this.orderRepo = orderRepo;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getById(Long id){
+        Optional<Order> order = orderRepo.findById(id);
+        if (order.isPresent()) return new ResponseEntity<>(order.get(), HttpStatus.OK);
+        return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{orderId}")
+    public Order putOrder(@RequestBody Order order) {
+        return orderRepo.save(order);
+    }
+
+    @PatchMapping(path="/{orderId}", consumes="application/json")
+    public Order patchOrder(@PathVariable("orderId") Long orderId,
+                            @RequestBody Order patch) {
+        Order order = orderRepo.findById(orderId).get();
+        if (patch.getName() != null) {
+            order.setName(patch.getName());
+        }
+        if (patch.getStreet() != null) {
+            order.setStreet(patch.getStreet());
+        }
+        if (patch.getCity() != null) {
+            order.setCity(patch.getCity());
+        }
+        if (patch.getState() != null) {
+            order.setState(patch.getState());
+        }
+        if (patch.getZip() != null) {
+            order.setZip(patch.getState());
+        }
+        if (patch.getCcNumber() != null) {
+            order.setCcNumber(patch.getCcNumber());
+        }
+        if (patch.getCcExpiration() != null) {
+            order.setCcExpiration(patch.getCcExpiration());
+        }
+        if (patch.getCcCVV() != null) {
+            order.setCcCVV(patch.getCcCVV());
+        }
+        return orderRepo.save(order);
+    }
+
+    @DeleteMapping("/{orderId}")
+    @ResponseStatus(code=HttpStatus.NO_CONTENT)
+    public void deleteById(@PathVariable ("orderId") Long orderId) {
+        try {
+            orderRepo.deleteById(orderId);
+        } catch (EmptyResultDataAccessException exception) {
+
+        }
+    }
+}
