58cfe1603f6c90a681e7d9d1a7e6121bc38615a4
alexej <door1975@yandex.ru>
2020-10-15 03:56:37 +0300

    Разбирал главу про Spring Integration. Надо сделать отдельныmaven-projects по taco-kitchen, taco-email, tako-kafka, и т.п. Надо больше на github смотреть.

---
+++ src/test/java/sia/TestEmailProperties.java
@@ -0,0 +1,12 @@
+package sia;
+
+import org.junit.Test;
+import ru.durnov.taco.email.EmailProperties;
+
+public class TestEmailProperties {
+
+    @Test
+    public void testThatGeneratedMailStringEqualsMyEmail(){
+        EmailProperties emailProperties = new EmailProperties();
+    }
+}
